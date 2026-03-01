0e4ff7bec3da339ec9bcedb4f1b95582420d39ce
Vivikiwi <info@vivikiwi.lt>
2026-03-01 11:57:14 +0200

    Add to card button added

--- components/AddToCartButton.tsx
+++ components/AddToCartButton.tsx
@@ -8,11 +8,13 @@ interface Props {
 }
 
 const AddToCartButton = ({ product, className }: Props) => {
+	const isOutOfStock = product?.stock === 0;
 	return (
 		<div className="w-full h-12 flex items-center">
 			<Button
+				disabled={isOutOfStock}
 				className={cn(
-					"w-full bg-transparent text-darkColo cursor-pointer shadow-none border border-darkColor/30 font-semibold tracking-wide hover:text-white hoverEffect", className)}
+					"w-full bg-transparent text-darkColor cursor-pointer shadow-none border border-darkColor/30 font-semibold tracking-wide hover:text-white hoverEffect", className)}
 			>
 				Add to cart
 			</Button>
